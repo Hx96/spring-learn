08bea893ca83f536dab8eb5337d2c34cc47314b4
Hx <xinghuang96@foxmail.com>
2022-06-13 23:56:54 +0800

    mybatis

--- hx-mybatis/src/main/java/org/hx/AppConfig.java
+++ hx-mybatis/src/main/java/org/hx/AppConfig.java
@@ -1,4 +1,36 @@
 package org.hx;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AppConfig {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "org/mybatis/example/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            Blog blog = (Blog) session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
+        }
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            BlogMapper mapper = session.getMapper(BlogMapper.class);
+            Blog blog = mapper.selectBlog(101);
+        }
+        DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
+        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        Environment environment = new Environment("development", transactionFactory, dataSource);
+        Configuration configuration = new Configuration(environment);
+//        configuration.addMapper(BlogMapper.class);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+    }
 }
