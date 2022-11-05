b3b03e630e628c312debaaa75b139ee336b3a069
kyle <xinghuang96@foxmail.com>
2022-11-05 17:16:39 +0800

    解决依赖

--- hx-mybatis/src/main/java/org/hx/AppConfig.java
+++ hx-mybatis/src/main/java/org/hx/AppConfig.java
@@ -16,21 +16,21 @@ import java.io.InputStream;
 public class AppConfig {
 
     public static void main(String[] args) throws IOException {
-        String resource = "org/mybatis/example/mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        try (SqlSession session = sqlSessionFactory.openSession()) {
-            Blog blog = (Blog) session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
-        }
-        try (SqlSession session = sqlSessionFactory.openSession()) {
-            BlogMapper mapper = session.getMapper(BlogMapper.class);
-            Blog blog = mapper.selectBlog(101);
-        }
-        DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
-        TransactionFactory transactionFactory = new JdbcTransactionFactory();
-        Environment environment = new Environment("development", transactionFactory, dataSource);
-        Configuration configuration = new Configuration(environment);
-//        configuration.addMapper(BlogMapper.class);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+//        String resource = "org/mybatis/example/mybatis-config.xml";
+//        InputStream inputStream = Resources.getResourceAsStream(resource);
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        try (SqlSession session = sqlSessionFactory.openSession()) {
+//            Blog blog = (Blog) session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
+//        }
+//        try (SqlSession session = sqlSessionFactory.openSession()) {
+//            BlogMapper mapper = session.getMapper(BlogMapper.class);
+//            Blog blog = mapper.selectBlog(101);
+//        }
+//        DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
+//        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+//        Environment environment = new Environment("development", transactionFactory, dataSource);
+//        Configuration configuration = new Configuration(environment);
+////        configuration.addMapper(BlogMapper.class);
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
     }
 }
