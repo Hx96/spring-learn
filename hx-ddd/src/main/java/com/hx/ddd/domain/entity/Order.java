e412ba5029168516c52ddceeddeb1165d704d908
kyle <xinghuang96@foxmail.com>
2024-07-28 16:24:09 +0800

    7月week4 #time 0.5h

--- hx-ddd/src/main/java/com/hx/ddd/domain/entity/Order.java
+++ hx-ddd/src/main/java/com/hx/ddd/domain/entity/Order.java
@@ -1,7 +1,9 @@
 package com.hx.ddd.domain.entity;
 
 import com.hx.ddd.domain.repository.Aggregate;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Order implements Aggregate<OrderId> {
     @Override
     public OrderId getId() {
